52ce3379fdfd4c4d30f0aa6738145c5022f06732
Mezba Faisal <148391178+mezbafaisal@users.noreply.github.com>
2024-01-23 23:05:33 +0600

    Delete recommendations_system.py

--- recommendations_system.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import seaborn as sns
-from matplotlib import pyplot as plt
-import warnings
-import os
-import importlib
-warnings.filterwarnings('ignore')
-sns.set_theme(color_codes=True)
