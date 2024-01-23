2ddf70c687d8283f5b108d68db3a19e80f1d27e7
Mezba Faisal <148391178+mezbafaisal@users.noreply.github.com>
2024-01-23 22:55:06 +0600

    Create personalized_content_recommendations.py

---
+++ personalized_content_recommendations.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+import warnings
+import os
+import importlib
+warnings.filterwarnings('ignore')
+sns.set_theme(color_codes=True)
